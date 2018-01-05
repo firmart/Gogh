a371904f0f9f16325e94385621660875dd5bcdc4
firmart <firminmartin24@gmail.com>
2018-01-05 01:04:33 +0100

    Update

--- themes/darkside.sh
+++ themes/darkside.sh
@@ -23,44 +23,5 @@ BACKGROUND_COLOR="#222324"   # Background Color
 FOREGROUND_COLOR="#bababa"   # Text
 CURSOR_COLOR="$FOREGROUND_COLOR" # Cursor
 PROFILE_NAME="Darkside"
-# =============================================================== #
-
-
-
-
-
-
-
-# =============================================================== #
-# | Apply Colors
-# ===============================================================|#
-function gogh_colors () {
-    echo ""
-    echo -e "\033[0;30m█████\\033[0m\033[0;31m█████\\033[0m\033[0;32m█████\\033[0m\033[0;33m█████\\033[0m\033[0;34m█████\\033[0m\033[0;35m█████\\033[0m\033[0;36m█████\\033[0m\033[0;37m█████\\033[0m"
-    echo -e "\033[0m\033[1;30m█████\\033[0m\033[1;31m█████\\033[0m\033[1;32m█████\\033[0m\033[1;33m█████\\033[0m\033[1;34m█████\\033[0m\033[1;35m█████\\033[0m\033[1;36m█████\\033[0m\033[1;37m█████\\033[0m"
-    echo ""
-}
-
-function curlsource() {
-    f=$(mktemp -t curlsource)
-    curl -o "$f" -s -L "$1"
-    source "$f"
-    rm -f "$f"
-}
-
-SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-PARENT_PATH="$(dirname "$SCRIPT_PATH")"
-
-gogh_colors
-if [ -e $PARENT_PATH"/apply-colors.sh" ]; then
-    source $PARENT_PATH"/apply-colors.sh"
-else
-        if [ $(uname) = "Darwin" ]; then
-        # OSX ships with curl and ancient bash
-        # Note: here, sourcing directly from curl does not work
-        curlsource https://raw.githubusercontent.com/Mayccoll/Gogh/master/apply-colors.sh
-    else
-        # Linux ships with wget
-        source <(wget -O - https://raw.githubusercontent.com/Mayccoll/Gogh/master/apply-colors.sh)
-    fi
-fi
+# Apply Colors
+source ../_apply_colors.sh
